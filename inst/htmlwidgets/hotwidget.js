2e73a48223c29de55f4bf9414cd3de6facb5a2ae
ramdhadage <ram.dhadage123@gmail.com>
2025-12-28 21:42:37 +0530

    added column type formating

--- inst/htmlwidgets/hotwidget.js
+++ inst/htmlwidgets/hotwidget.js
@@ -17,13 +17,55 @@ HTMLWidgets.widget({
         const originalColHeaders = x.colHeaders || Object.keys(currentData[0] || {});
         const colHeaders = originalColHeaders.map((header) => header.toUpperCase());
         const colTypes = x.colTypes || {};
+        console.log('Column types:', colTypes);
         el.innerText = "";
         hotInstance = new Handsontable(el, {
           data: currentData,
           colHeaders: colHeaders,
+           className: 'htCenter',
           rowHeaders: false,
           columnSorting: true,
           contextMenu: ['undo', 'redo'],
+          manualColumnResize: true,
+          stretchH: 'all',
+           columns: originalColHeaders.map((colName) => {
+            const colType = colTypes[colName];
+            const config = { data: colName };
+
+            if (colName === 'MODEL') {
+              config.readOnly = true;
+            }
+            switch(colType) {
+              case 'numeric':
+                config.type = 'numeric';
+                config.numericFormat = {
+                  pattern: '0,0.00'
+                };
+                break;
+
+              case 'integer':
+                config.type = 'numeric';
+                config.numericFormat = {
+                  pattern: '0,0'
+                };
+                break;
+
+              case 'character':
+                config.type = 'text';
+                break;
+
+             case 'logical':
+                config.type = 'checkbox';
+                break;
+             case 'factor':
+                config.type = 'select';
+                config.selectOptions = [...new Set(x.data[colName])];
+                break;
+            default:
+                config.type = 'text';
+            }
+            return config;
+          }),
           licenseKey: 'non-commercial-and-evaluation',
         })
 
