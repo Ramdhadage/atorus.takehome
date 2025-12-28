dda67c00a9a4bfdaca95deafeecd53b221baece0
ramdhadage <ram.dhadage123@gmail.com>
2025-12-29 01:55:39 +0530

    call module mod_table in app server

--- R/app_server.R
+++ R/app_server.R
@@ -7,4 +7,7 @@
 app_server <- function(input, output, session) {
   # Your application server logic
   store <- DataStore$new()
+  store_reactive <- reactiveVal(store)
+  store_trigger <- reactiveVal(0)
+  mod_table_server("table", store_reactive, store_trigger)
 }
