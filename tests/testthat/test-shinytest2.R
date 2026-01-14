7df0cd9b7eb3690c39929c4dad22922f31413500
ramdhadage <ram.dhadage123@gmail.com>
2026-01-14 08:00:54 +0530

    Update app launch test to include default values and adjust wait times

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,12 +1,15 @@
 library(shinytest2)
+library(atorus.takehome)
 # Helper to start app driver with safe teardown
 start_app <- function(...) {
-  app <- AppDriver$new(app_dir = "../../", load_timeout = 20000, variant = platform_variant(), width = 1200, height = 1600)
+  app <- AppDriver$new(app_dir = "../../", load_timeout = 20000, variant = platform_variant(), expect_values_screenshot_args = FALSE, width = 1200, height = 1600)
 
   app
 }
-test_that("app launches", {
+test_that("app launches with default values", {
   app <- start_app()
-  app$wait_for_idle(1000)
-  app$expect_screenshot(name = "app-launches")
+  app$wait_for_idle(1500)
+  # app$wait_for_value(output = "table-table", timeout = 10000)
+  app$wait_for_idle(1500)
+  app$expect_values(name = "app-launches")
 })
