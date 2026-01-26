7134a1f99261666163525fa8e56eec1be74159a3
ramdhadage <ram.dhadage123@gmail.com>
2026-01-27 00:51:04 +0530

    uncomment the shinytest2 test

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,7 +1,8 @@
-# test_that("app launches with default values", {
-#   app <- start_app()
-#   app$wait_for_idle(1500)
-#   # app$wait_for_value(output = "table-table", timeout = 10000)
-#   app$wait_for_idle(1500)
-#   app$expect_values(name = "app-launches")
-# })
+test_that("app launches with default values", {
+  app <- start_app()
+  app$wait_for_idle(1500)
+  # app$wait_for_value(output = "table-table", timeout = 10000)
+  app$wait_for_idle(1500)
+  app$expect_values(name = "app-launches")
+  #app$expect_screenshot(name = "app-launches")
+})
