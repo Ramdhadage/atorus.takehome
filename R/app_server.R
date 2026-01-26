56f08765520292ce4f048c49350f7f8b1c458dad
ramdhadage <ram.dhadage123@gmail.com>
2026-01-26 16:38:41 +0530

    Refactor app_server to utilize cached store and remove redundant initialization logic

--- R/app_server.R
+++ R/app_server.R
@@ -1,6 +1,6 @@
 .datastore_cache <- NULL
 .datastore_init_time <- Sys.time()
-
+.cache_env <- new.env()
 #' The application server-side
 #'
 #' @param input,output,session Internal parameters for {shiny}.
@@ -8,13 +8,7 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-  if (is.null(.datastore_cache)) {
-    .datastore_cache <<- DataStore$new()
-    .datastore_init_time <<- Sys.time()
-  } else {
-    .datastore_cache$revert()
-  }
-  store <- .datastore_cache
+  store <- get_cached_store()
   store_reactive <- reactiveVal(store)
   store_trigger <- reactiveVal(0)
   mod_table_server("table", store_reactive, store_trigger)
