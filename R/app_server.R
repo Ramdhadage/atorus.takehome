264eb8a19860174f391aefb24fff98ac4a38806b
ramdhadage <ram.dhadage123@gmail.com>
2026-01-26 14:37:24 +0530

    Refactor app_server to implement datastore caching and initialization logic

--- R/app_server.R
+++ R/app_server.R
@@ -1,3 +1,6 @@
+.datastore_cache <- NULL
+.datastore_init_time <- Sys.time()
+
 #' The application server-side
 #'
 #' @param input,output,session Internal parameters for {shiny}.
@@ -5,8 +8,13 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-  # Your application server logic
-  store <- DataStore$new()
+  if (is.null(.datastore_cache)) {
+    .datastore_cache <<- DataStore$new()
+    .datastore_init_time <<- Sys.time()
+  } else {
+    .datastore_cache$revert()
+  }
+  store <- .datastore_cache
   store_reactive <- reactiveVal(store)
   store_trigger <- reactiveVal(0)
   mod_table_server("table", store_reactive, store_trigger)
