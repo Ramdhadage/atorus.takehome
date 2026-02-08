c07d0fae7ed8e08d08dee46d2044067cdc418d70
ramdhadage <ram.dhadage123@gmail.com>
2026-02-08 23:07:37 +0530

    feat: add initial Shiny app setup and update test helper to point to app directory

--- tests/testthat/helper-shinytest2.R
+++ tests/testthat/helper-shinytest2.R
@@ -1,12 +1,9 @@
-# Load application support files into testing environment
-shinytest2::load_app_support(app_dir = here::here())
-
 # Explicitly load the package to ensure htmlwidgets and other dependencies are available
 library(editable)
 library(shiny)
 start_app <- function(...) {
   app <- shinytest2::AppDriver$new(
-    app_dir = here::here(), 
+    app_dir = here::here("inst/app"), 
     load_timeout = 20000, 
     variant = shinytest2::platform_variant(), 
     expect_values_screenshot_args = FALSE, 
