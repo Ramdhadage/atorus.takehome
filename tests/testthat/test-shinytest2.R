60bfe762c9712ce1a434d84ad00e6e5397fb52c0
ramdhadage <ram.dhadage123@gmail.com>
2026-02-08 13:16:48 +0530

    minor update

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,8 +1,8 @@
 test_that("app launches with default values", {
   app <- start_app()
-  app$wait_for_idle(1500)
-  # app$wait_for_value(output = "table-table", timeout = 10000)
-  app$wait_for_idle(1500)
+  app$wait_for_idle(150)
+  app$wait_for_value(output = "table-table", timeout = 10000)
+  app$wait_for_idle(150)
   app$expect_values(name = "app-launches")
-  #app$expect_screenshot(name = "app-launches")
+  app$expect_screenshot(name = "app-launches")
 })
